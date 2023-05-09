c31597d201508fb8f90d8ec1a7b32021db0ec4be
nrisa <risanussy07@gmail.com>
2023-05-09 20:31:36 +0700

    ok

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavbarRoot />
+  <router-view />
+</template>
+
+<script>
+  import NavbarRoot from "./components/NavbarRoot.vue";
+
+  export default {
+    name: "App",
+    components: {
+      NavbarRoot,
+    },
+  }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <br><br>
+  <div class="container">
+    <h1>Daftar Karyawan.</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+};
+</script>
